f1ee095e558fbd86473bd875419c8225ec280bda
Pavel Revak <pavel.revak@gmail.com>
2021-11-20 01:53:32 +0100

    Reworked / simplied command line processing, handling errors, improved logging (allow -vv to increase log level), code is ready for configuration files with multiple serial ports

--- README.md
+++ README.md
@@ -10,18 +10,23 @@ Simple proxy for connecting over TCP or telnet to serial port
 - TCP:
   - allow to set listening socket (address and port, default: localhost:10000, change with: ```-l``` or ```--listen``` param)
   - telnet mode (send every character immediately and not waiting to enter, with ```-t``` or ```--telnet``` param)
-  - allow more connections to one serial port:
+  - allow multiple connections to one serial port:
     - each connected client can sent to serial
     - serial send received data to all connected clients
 
 ## Usage examples:
 ```
-python ser2tcp.py /dev/ttyS0 9600
-python ser2tcp.py /dev/tty.SLAB_USBtoUART 115200 EVEN -l 0:10001 -t -v
-python ser2tcp.py COM1 115200 ODD TWO -l 0:10001 -v
+python ser2tcp.py localhost 10001 /dev/ttyS0 9600
+python ser2tcp.py 0 12345 telnet /dev/tty.SLAB_USBtoUART 115200 EVEN ONE
+python ser2tcp.py 0 10001 COM1 115200 ODD TWO
 python ser2tcp.py --help                                              
 ```
 
 ## Requirements
-- python v3.4+
+- python v3.5+
 - pyserial v3.0+
+
+### Running on:
+- Linux
+- MacOS
+- Windows
--- ser2tcp.py
+++ ser2tcp.py
@@ -7,12 +7,21 @@ import select
 import socket
 import argparse
 import logging
+import signal
 import serial
+
+
+def sigterm_handler(_signo, _stack_frame):
+    """Raises SystemExit(0)"""
+    sys.exit(0)
+
+
+class ConfigError(Exception):
+    """Configuration error exception"""
 
 
 class Ser2TcpConnection():
     """Telnet socket"""
-
     def __init__(self, connection, ser, telnet=False, log=None):
         self._socket, self._addr = connection
         self._serial = ser
@@ -21,7 +30,9 @@ class Ser2TcpConnection():
         if self._telnet:
             self.send((0xff, 0xfd, 0x22))
             self.send((0xff, 0xfb, 0x01))
-        self._log.info("Client connected: %s:%d", self._addr[0], self._addr[1])
+        self._log.info(
+            "Client connected: %s:%d%s",
+            *self._addr, " TELNET" if self._telnet else "")
 
     def __del__(self):
         self.close()
@@ -36,7 +47,7 @@ class Ser2TcpConnection():
         if self._socket:
             self._socket.close()
             self._socket = None
-            self._log.info("Client disconnected: %s:%d", self._addr[0], self._addr[1])
+            self._log.info("Client disconnected: %s:%d", *self._addr)
 
     def fileno(self):
         """emulate fileno method of socket"""
@@ -58,21 +69,20 @@ class Ser2TcpConnection():
         del data[0]
         return dat
 
-    @staticmethod
-    def filter_telnet_commands(data):
+    @classmethod
+    def filter_telnet_commands(cls, data):
         """Remove telnet commands from data"""
-
         out = []
         subnegotiation = False
         sbn = []
 
         # process telnet commands
         while data:
-            dat = Ser2TcpConnection.list_pull_first(data)
+            dat = cls.list_pull_first(data)
             if dat == 255:
-                dat = Ser2TcpConnection.list_pull_first(data)
+                dat = cls.list_pull_first(data)
                 if dat in (251, 252, 253, 254):
-                    dat = Ser2TcpConnection.list_pull_first(data)
+                    dat = cls.list_pull_first(data)
                     continue
                 if dat == 250:
                     subnegotiation = True
@@ -93,49 +103,28 @@ class Ser2TcpConnection():
         """Received data from client"""
         data = list(data)
         if self._telnet:
-            data = Ser2TcpConnection.filter_telnet_commands(data)
+            data = self.filter_telnet_commands(data)
         if data:
             self._serial.write(data)
+
+    def get_address(self):
+        """Return address"""
+        return self._addr
 
 
 class Ser2TcpServer():
     """Telnet server"""
-
-    def __init__(self, bind_address, serial_params, telnet=False, log=None):
-        """Ser2TcpServer start ser2Tcp server
-        Arguments:
-            bind_address: ip_address and port to bind (address:port), default: 127.0.0.1:10000
-            serial_params: ip_address and port to bind (address:port), default: 127.0.0.1:10000
-            telnet: if true use telnet mode for clients, instead raw TCP connection is used
-            log: logging.Logger instance for logging
-        """
+    def __init__(self, config, log=None):
+        """Ser2TcpServer start ser2Tcp server"""
         self._log = log if log else logging.Logger("Ser2TcpServer")
-        self._log.info(
-            "Starting server (listen: %s, serial: %s:%d)..",
-            bind_address, serial_params['port'], serial_params['baud'])
-
-        address, port = bind_address.split(':')
-        if not address:
-            address = '127.0.0.1'
-        port = int(port) if port else 10000
-        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
-        self._socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self._socket.bind((address, port))
-        self._socket.listen(1)
+        self._log.info("Starting server (%s)..", config)
+        self._config = config
         self._connections = []
         self._serial = None
-        self._serial_params = serial_params
-        parity = serial.PARITY_NONE
-        stopbits = serial.STOPBITS_ONE
-        if 'EVEN' in serial_params['flags']:
-            parity = serial.PARITY_EVEN
-        elif 'ODD' in serial_params['flags']:
-            parity = serial.PARITY_ODD
-        if 'TWO' in serial_params['flags']:
-            stopbits = serial.STOPBITS_TWO
-        self._serial_params['parity'] = parity
-        self._serial_params['stopbits'] = stopbits
-        self._telnet = telnet
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
+        self._socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self._socket.bind(config.get_bind_address())
+        self._socket.listen(1)
 
     def __del__(self):
         self.close()
@@ -144,152 +133,242 @@ class Ser2TcpServer():
         if self._serial:
             return True
         try:
-            self._serial = serial.Serial(
-                port=self._serial_params['port'],
-                baudrate=self._serial_params['baud'],
-                parity=self._serial_params['parity'],
-                stopbits=self._serial_params['stopbits'],
-            )
+            self._serial = serial.Serial(**self._config.get_serial_config())
         except (serial.SerialException, OSError) as err:
-            self._log.warning("Serial %s is not connected %s", self._serial_params['port'], err)
+            self._log.warning(
+                "Serial %s is not connected [%s]",
+                self._config.get_serial_port(), err)
             return False
-        self._log.info("Serial %s connected", self._serial_params['port'])
+        self._log.info(
+            "Serial %s connected", self._config.get_serial_port())
         return True
 
     def _serial_disconnect(self):
         if self._serial:
             self._serial.close()
             self._serial = None
-            self._log.info("Serial %s disconnected", self._serial_params['port'])
+            self._log.info(
+                "Serial %s disconnected", self._config.get_serial_port())
 
     def _client_connect(self):
         sock, addr = self._socket.accept()
         if not self._connections and not self._serial:
             if not self._serial_connect():
-                self._log.info("Client canceled: %s:%d", socket, addr)
+                self._log.info("Client canceled: %s:%d", *addr)
                 sock.close()
                 return
         ser2tcp_connection = Ser2TcpConnection(
             connection=(sock, addr),
             ser=self._serial,
-            telnet=self._telnet,
+            telnet=self._config.is_telnet(),
             log=self._log,
         )
         self._connections.append(ser2tcp_connection)
 
     def _clients_disconnect(self):
-        for con in self._connections:
-            con.close()
-        self._connections = []
+        while self._connections:
+            self._connections.pop().close()
 
     def close(self):
         """Close socket and all connections"""
-        self._log.info("Exiting..")
         self._clients_disconnect()
         self._serial_disconnect()
         if self._socket is not None:
             self._socket.close()
             self._socket = None
 
-    def process(self):
-        """Task start"""
+    def sockets(self):
+        """Return all sockets from this server"""
         sockets = [self._socket] + self._connections
         if self._serial:
             sockets.append(self._serial)
-        read_sockets = select.select(sockets, [], [], .1)[0]
-        if not read_sockets:
-            return
+        return sockets
+
+    def socket_event(self, read_sockets):
+        """Process sockets with read event"""
         if self._socket in read_sockets:
             self._client_connect()
             return
-        raw_data = None
-        if self._serial in read_sockets:
+        serial_data = None
+        if self._serial and self._serial in read_sockets:
             try:
-                raw_data = self._serial.read()
+                serial_data = self._serial.read()
+                self._log.debug("(%s): %s", self._config.get_serial_port(), serial_data)
             except serial.SerialException:
                 self._clients_disconnect()
                 self._serial_disconnect()
                 return
         for con in self._connections:
             if con in read_sockets:
-                data = con.socket.recv(4096)
-                if not data:
+                con_data = con.socket.recv(4096)
+                self._log.debug("(%s:%d): %s", *con.get_address(), con_data)
+                if not con_data:
                     con.close()
                     self._connections.remove(con)
                     if not self._connections:
                         self._serial_disconnect()
                     return
-                con.on_received(data)
-            if raw_data:
-                con.send(raw_data)
+                con.on_received(con_data)
+            if serial_data:
+                con.send(serial_data)
 
 
-VERSION_STR = "ser2tcp v1.0.0"
+class ServersManager():
+    """Servers manager"""
+    def __init__(self):
+        self._servers = []
+
+    def add_server(self, server):
+        """Add server"""
+        self._servers.append(server)
+
+    def process(self):
+        """Process all servers"""
+        sockets = []
+        for server in self._servers:
+            sockets.extend(server.sockets())
+        read_sockets = select.select(sockets, [], [], .1)[0]
+        if read_sockets:
+            for server in self._servers:
+                server.socket_event(read_sockets)
+
+    def close(self):
+        """Close all servers"""
+        for server in self._servers:
+            server.close()
+
+
+class Config():
+    """Configuration"""
+    def __init__(self, entry):
+        if not entry:
+            raise ConfigError("Empty configuration")
+        self._host = entry.pop(0)
+        if not entry:
+            raise ConfigError("Missing port")
+        if not entry[0].isnumeric():
+            raise ConfigError("Wrong port number")
+        self._port = int(entry.pop(0))
+        if not entry:
+            raise ConfigError("Missing serial port")
+        self._mode = 'RAW'
+        if entry[0].upper() in ('RAW', 'TELNET'):
+            self._mode = entry.pop(0).upper()
+        if not entry:
+            raise ConfigError("Missing serial port")
+        self._serial_port = entry.pop(0)
+        if not entry:
+            raise ConfigError("Missing baud-rate")
+        if not entry[0].isnumeric():
+            raise ConfigError("Wrong baud-rate")
+        self._baud_rate = int(entry.pop(0))
+        self._parity = 'NONE'
+        if entry and entry[0].upper() in ('NONE', 'EVEN', 'ODD'):
+            self._parity = entry.pop(0)
+        self._stop_bits = 'ONE'
+        if entry and entry[0].upper() in ('ONE', 'TWO'):
+            self._stop_bits = entry.pop(0)
+        if entry:
+            raise ConfigError("Too many parameters")
+
+    def __str__(self):
+        return ' '.join((
+            self._host + ":" + str(self._port),
+            self._mode,
+            self._serial_port,
+            str(self._baud_rate),
+            self._parity,
+            self._stop_bits))
+
+    def get_bind_address(self):
+        """Return bind address for TCP server"""
+        return (self._host, self._port)
+
+    def is_telnet(self):
+        """Return True if connection emulates telnet server"""
+        return self._mode == 'TELNET'
+
+    def get_serial_config(self):
+        """Return configuration parameters for serial"""
+        parity = serial.PARITY_NONE
+        stop_bits = serial.STOPBITS_ONE
+        if 'EVEN' in self._parity:
+            parity = serial.PARITY_EVEN
+        elif 'ODD' in self._parity:
+            parity = serial.PARITY_ODD
+        if 'TWO' in self._stop_bits:
+            stop_bits = serial.STOPBITS_TWO
+        return {
+            'port': self._serial_port,
+            'baudrate': self._baud_rate,
+            'parity': parity,
+            'stopbits': stop_bits,
+        }
+
+    def get_serial_port(self):
+        """Return serial port """
+        return self._serial_port
+
+
+VERSION_STR = "ser2tcp v2.0.0"
 
 DESCRIPTION_STR = VERSION_STR + """
-(c) 2016 by pavel.revak@gmail.com
+(c) 2016-2021 by pavel.revak@gmail.com
 https://github.com/pavelrevak/ser2tcp
 """
 
+
 def main():
     """Main"""
-    # test version of python
-    if sys.version_info < (3, 4):
-        print("Wrong python version, required is at lease 3.4")
-        exit(1)
-    # test pyserial version
-    pyserial_version = [int(i) for i in serial.__version__.split('.')]
-    if pyserial_version[0] < 3:
-        print("Wrong pyserial version, required is at lease 3.0")
-        exit(1)
-
+    signal.signal(signal.SIGTERM, sigterm_handler)
+    signal.signal(signal.SIGINT, sigterm_handler)
     parser = argparse.ArgumentParser(description=DESCRIPTION_STR)
     parser.add_argument('-V', '--version', action='version', version=VERSION_STR)
     parser.add_argument(
         '-v', '--verbose', action='count', default=0,
         help="Increase verbosity *")
     parser.add_argument(
-        '-t', '--telnet', action='store_true', default=0,
-        help="telnet (use telnet protocol)")
-    parser.add_argument(
-        '-l', '--listen', default='127.0.0.1:10000',
-        help="Listen host (default: 127.0.0.1:10000)")
-    parser.add_argument(
-        'port',
-        help="Serial port")
-    parser.add_argument(
-        'baud', type=int,
-        help="Serial baud rate")
-    parser.add_argument(
-        'flags', nargs='*', choices=['NONE', 'EVEN', 'ODD', 'ONE', 'TWO'],
-        help="Serial parameters: parity: [NONE|EVEN|ODD], stop bits: [ONE|TWO], default: NONE ONE")
+        'connection', nargs=argparse.REMAINDER,
+        help="{host} {port} [RAW|TELNET]"
+        " {serial_port} {baud_rate} [NONE|EVEN|ODD] [ONE|TWO]")
     args = parser.parse_args()
 
-    logging.basicConfig(format='%(asctime)-15s %(message)s')
+    logging.basicConfig(format='%(asctime)-15s %(levelname)s : %(message)s')
     log = logging.getLogger('ser2tcp')
-    if args.verbose:
-        log.setLevel(10)
+    verbose_level = min(2, args.verbose)
+    log.setLevel((30, 20, 10)[verbose_level])
 
-    ser2tcp = Ser2TcpServer(
-        bind_address=args.listen,
-        serial_params={
-            'port': args.port,
-            'baud': args.baud,
-            'flags': args.flags,
-        },
-        telnet=args.telnet,
-        log=log,
-    )
+    # check version of python
+    if sys.version_info < (3, 6):
+        log.error("Wrong python version, required is at lease 3.5")
+        sys.exit(1)
+    # check pyserial version
+    pyserial_version = [int(i) for i in serial.__version__.split('.')]
+    if pyserial_version[0] < 3:
+        log.error("Wrong pyserial version, required is at lease 3.0")
+        sys.exit(1)
+
+    connections = []
     try:
+        if args.connection:
+            connections.append(Config(args.connection))
+        if not connections:
+            raise ConfigError("No connection configured")
+    except ConfigError as err:
+        log.error(err)
+        sys.exit(0)
+
+    servers_manager = ServersManager()
+    try:
+        for config in connections:
+            servers_manager.add_server(Ser2TcpServer(config, log))
         while True:
-            ser2tcp.process()
-    except Exception as err:
-        print("%s" % err)
-        raise err
-    except KeyboardInterrupt:
-        pass
+            servers_manager.process()
+    except OSError as err:
+        log.info(err)
     finally:
-        ser2tcp.close()
+        log.info("Exiting..")
+        servers_manager.close()
 
 
 if __name__ == '__main__':
